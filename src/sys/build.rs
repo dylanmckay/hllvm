90131bcac42bdbd19e7e68c946bcc71a17cb770e
Dylan McKay <dylanmckay34@gmail.com>
2016-12-18 12:57:39 +1300

    Use llvm-config in build script

--- src/sys/build.rs
+++ src/sys/build.rs
@@ -1,22 +1,21 @@
 extern crate gcc;
 
-// llvm-config --libs
-const LLVM_LIBS: &'static [&'static str] = &[
-  "LLVMObjCARCOpts", "LLVMSymbolize", "LLVMDebugInfoPDB",
-  "LLVMDebugInfoDWARF", "LLVMGlobalISel", "LLVMCoverage", "LLVMTableGen",
-  "LLVMOrcJIT", "LLVMMIRParser", "LLVMNVPTXCodeGen", "LLVMNVPTXDesc", "LLVMNVPTXInfo",
-  "LLVMNVPTXAsmPrinter", "LLVMARMDisassembler", "LLVMARMCodeGen", "LLVMARMAsmParser",
-  "LLVMARMDesc","LLVMARMInfo", "LLVMARMAsmPrinter", "LLVMAMDGPUDisassembler", "LLVMAMDGPUCodeGen",
-  "LLVMAMDGPUAsmParser", "LLVMAMDGPUDesc", "LLVMAMDGPUInfo", "LLVMAMDGPUAsmPrinter",
-  "LLVMAMDGPUUtils", "LLVMObjectYAML", "LLVMX86Disassembler", "LLVMX86AsmParser",
-  "LLVMX86CodeGen", "LLVMSelectionDAG", "LLVMAsmPrinter", "LLVMDebugInfoCodeView",
-  "LLVMX86Desc", "LLVMMCDisassembler", "LLVMX86Info", "LLVMX86AsmPrinter", "LLVMX86Utils",
-  "LLVMMCJIT", "LLVMLibDriver", "LLVMOption", "LLVMLineEditor", "LLVMPasses", "LLVMipo",
-  "LLVMVectorize", "LLVMLinker", "LLVMIRReader", "LLVMAsmParser", "LLVMInterpreter",
-  "LLVMExecutionEngine", "LLVMRuntimeDyld", "LLVMObject", "LLVMMCParser", "LLVMCodeGen",
-  "LLVMTarget", "LLVMScalarOpts", "LLVMInstCombine", "LLVMInstrumentation", "LLVMTransformUtils",
-  "LLVMMC", "LLVMBitWriter", "LLVMBitReader", "LLVMAnalysis", "LLVMProfileData", "LLVMCore", "LLVMSupport"
-];
+use std::process;
+
+fn llvm_config(args: &[&str]) -> String {
+    let output = process::Command::new("llvm-config").args(args).output().unwrap();
+    assert!(output.status.success(), "llvm-config was unsuccessful");
+    String::from_utf8(output.stdout).expect("invalid utf-8 from llvm-config")
+}
+
+fn find_llvm_include_dir() -> String { llvm_config(&["--includedir"]) }
+fn find_llvm_lib_dir() -> String { llvm_config(&["--libdir"]) }
+
+fn llvm_link_libraries() -> Vec<String> {
+    llvm_config(&["--libs"]).split_whitespace()
+        .map(|l| l.trim_left_matches("-l").to_owned()) // Remove the '-l' linker arg
+        .collect()
+}
 
 fn main() {
     gcc::Config::new()
@@ -31,11 +30,12 @@ fn main() {
         .file("ir/inst.cpp")
         .file("ir/context.cpp")
         .file("ir/module.cpp")
+        .include(find_llvm_include_dir())
         .compile("libllvm-sys.a");
 
-    println!("cargo:rustc-link-search=native=/usr/local/Cellar/llvm/3.9.0/lib");
+    println!("cargo:rustc-link-search=native={}", find_llvm_lib_dir());
 
-    for library in LLVM_LIBS {
+    for library in llvm_link_libraries() {
         println!("cargo:rustc-link-lib=static={}", library);
     }
 }
