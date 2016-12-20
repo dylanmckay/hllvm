6e9c08bb02529e5e3b747a23ec928fb4435e33db
Dylan McKay <dylanmckay34@gmail.com>
2016-12-20 13:23:27 +1300

    Add stubs for all the unimplemented instructions

--- src/ir/value/user/inst/mod.rs
+++ src/ir/value/user/inst/mod.rs
@@ -6,3 +6,58 @@ use ir::User;
 
 pub struct Instruction<'ctx>(User<'ctx>);
 impl_upcast!(Instruction => User);
+
+macro_rules! define_unimplemented_inst {
+    ($name:ident) => {
+        /// Unimplemented instruction.
+        pub struct $name<'ctx>(Instruction<'ctx>);
+
+        impl_upcast!($name => Instruction);
+    }
+}
+
+define_unimplemented_inst!(AtomicCmpXchgInst);
+define_unimplemented_inst!(AtomicRMWInst);
+define_unimplemented_inst!(BinaryOperator);
+define_unimplemented_inst!(CallInst);
+define_unimplemented_inst!(IntrinsicInst);
+define_unimplemented_inst!(CmpInst);
+define_unimplemented_inst!(FCmpInst);
+define_unimplemented_inst!(ICmpInst);
+define_unimplemented_inst!(ExtractElementInst);
+define_unimplemented_inst!(FenceInst);
+define_unimplemented_inst!(FuncletPadInst);
+define_unimplemented_inst!(CatchPadInst);
+define_unimplemented_inst!(CleanupPadInst);
+define_unimplemented_inst!(GetElementPtrInst);
+define_unimplemented_inst!(InsertElementInst);
+define_unimplemented_inst!(InsertValueInst);
+define_unimplemented_inst!(LandingPadInst);
+define_unimplemented_inst!(SelectInst);
+define_unimplemented_inst!(ShuffleVectorInst);
+define_unimplemented_inst!(StoreInst);
+define_unimplemented_inst!(CatchReturnInst);
+define_unimplemented_inst!(CatchSwitchInst);
+define_unimplemented_inst!(CleanupReturnInst);
+define_unimplemented_inst!(IndirectBrInst);
+define_unimplemented_inst!(InvokeInst);
+define_unimplemented_inst!(ResumeInst);
+define_unimplemented_inst!(SwitchInst);
+define_unimplemented_inst!(UnreachableInst);
+define_unimplemented_inst!(AllocaInst);
+define_unimplemented_inst!(AddrSpaceCastInst);
+define_unimplemented_inst!(BitCastInst);
+define_unimplemented_inst!(FPExtInst);
+define_unimplemented_inst!(FPToSIInst);
+define_unimplemented_inst!(FPToUIInst);
+define_unimplemented_inst!(FPTruncInst);
+define_unimplemented_inst!(IntToPtrInst);
+define_unimplemented_inst!(PtrToIntInst);
+define_unimplemented_inst!(SExtInst);
+define_unimplemented_inst!(SIToFPInst);
+define_unimplemented_inst!(TruncInst);
+define_unimplemented_inst!(UIToFPInst);
+define_unimplemented_inst!(ZExtInst);
+define_unimplemented_inst!(ExtractValueInst);
+define_unimplemented_inst!(LoadInst);
+define_unimplemented_inst!(VAArgInst);
